02bdd7a871910bd25037760a478428209f7c5e3a
Masahiro Nakagawa <masahiro.nakagawa@canonical.com>
2023-10-03 17:23:03 +0900

    Initial Commit

---
+++ testsnapC_sendrecv/stage/C_server_unix_dgram.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import socket
+
+
+def srv_sock_file():
+    socket_path = '/var/snap/testsnap-c-sendrecv/current/export/unix_socket_recvC'
+    datasize = 1024
+
+    print("socket_path:{}".format(socket_path), flush=True)
+
+    if os.path.exists(socket_path):
+        os.remove(socket_path)
+
+    try:
+        with socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM) as s:
+            s.bind(socket_path)
+
+            while True:
+                print("wait recv", flush=True)
+                recvdata = s.recv(datasize)
+
+                data = recvdata.decode()
+                print("received : {}bytes.\ndata={}".format(len(recvdata), data), flush=True)
+
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt", flush=True)
+
+    finally:
+        if os.path.exists(socket_path):
+            os.remove(socket_path)
+
+
+if __name__ == '__main__':
+    srv_sock_file()
+
